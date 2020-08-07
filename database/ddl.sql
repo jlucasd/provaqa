42d587ace27708de565a58f274272be2ac0cad0e
João Lucas Damiani <jlucasd01@gmail.com>
2020-08-07 20:09:31 -0300

    Update ddl.sql

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,4 +1,3 @@
 alter table matricula add confirmacao date default current_date;
-alter table unidade add ativa boolean default true;
-alter table curso add numerovagas int default 0;
 alter table matricula add situacao varchar(255) default 'Concluído';
+alter table curso add numerovagas int default 0;
